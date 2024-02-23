e954c7bf842d45aa3fefa03eb7fb8950b04c8cf1
unknown <omolloashley2@gmail.com>
2024-02-23 12:36:34 +0300

    first commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("How old are you?")
+location = input("Where do you live?")
+print(f"Hello {name}!Its great to know that you're {age} and located in {location}.Have a wonderful day !")
